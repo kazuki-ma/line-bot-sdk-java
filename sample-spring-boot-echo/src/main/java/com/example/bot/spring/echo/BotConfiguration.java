2df49ed8b93f60fcfc0d2bd40c6495459cc64645
Kazuki MATSUDA <matsuda.kazuki@gmail.com>
2017-01-09 22:34:48 +0900

    Update OG Image

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/BotConfiguration.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/BotConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
-@Component
+@Component("config")
 @Data
 @ConfigurationProperties(prefix = "")
 public class BotConfiguration {
